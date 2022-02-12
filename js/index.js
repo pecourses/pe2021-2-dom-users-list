09dacc713f06d06c2c12ba6849cf0c39461709e1
Viktoriia <pecourses@gmail.com>
2022-02-12 18:29:38 +0200

    Add styles

---
+++ js/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const user = {
+  firstName: 'Test',
+  lastName: 'Testovich',
+  age: 28,
+  photoSrc:
+    'https://www.whatsappprofiledpimages.com/wp-content/uploads/2021/08/Profile-Photo-Wallpaper.jpg',
+};
